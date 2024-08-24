bd0e5aa0b4e8abbc5eaf7ac7a2caf206bd8130fa
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2024-08-23 21:32:44 -0300

    Criando a tela inicial do sistema

---
+++ src/main/java/com/github/henriquemb/fornecedor_uninter/controller/AplicacaoController.java
@@ -0,0 +1,17 @@
+package com.github.henriquemb.fornecedor_uninter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AplicacaoController {
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value = "/login")
+    public String login() {
+        return "login";
+    }
+}
