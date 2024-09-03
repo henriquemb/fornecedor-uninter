de5436e401ba4b466159e7170c671ace8d77e821
Moacir Henrique Morais Baruffi <moacirhenrique01@hotmail.com>
2024-09-03 00:21:12 -0300

    Criado o rest controller do produto estoque

---
+++ src/main/java/com/github/henriquemb/fornecedor_uninter/api/ProdutoEstoqueRestController.java
@@ -0,0 +1,49 @@
+package com.github.henriquemb.fornecedor_uninter.api;
+
+import com.github.henriquemb.fornecedor_uninter.bo.ProdutoBO;
+import com.github.henriquemb.fornecedor_uninter.bo.ProdutoEstoqueBO;
+import com.github.henriquemb.fornecedor_uninter.model.ProdutoEstoque;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/api/estoque")
+public class ProdutoEstoqueRestController {
+    @Autowired
+    private ProdutoEstoqueBO bo;
+
+    @Autowired
+    private ProdutoBO pbo;
+
+    @GetMapping
+    public List<ProdutoEstoque> buscarTodos() {
+        return bo.buscarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ProdutoEstoque buscarPorId(@PathVariable Long id) {
+        return bo.buscarPorId(id);
+    }
+
+    @PostMapping
+    public ProdutoEstoque inserir(@RequestBody ProdutoEstoque produtoEstoque) {
+        bo.inserir(produtoEstoque);
+        return produtoEstoque;
+    }
+
+    @PutMapping("/{id}")
+    public ProdutoEstoque atualizar(@PathVariable Long id, @RequestBody ProdutoEstoque produtoEstoque) {
+        produtoEstoque.setId(id);
+        produtoEstoque.setProduto(pbo.buscarPorId(produtoEstoque.getProduto().getId()));
+
+        bo.atualizar(produtoEstoque);
+
+        return produtoEstoque;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        bo.deletar(id);
+    }
+}
